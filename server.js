464750983d68c5466fa76c8ed7acfe556f39218e
vanype <islam.baymuldin.o@gmail.com>
2025-03-27 00:56:06 +0500

    dbInit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 
+const pool = require("./db"); // Импорт модуля с БД
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +13,15 @@ app.use(express.static("public"));
 // Обрабатываем корневой маршрут
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW();");
+        res.json({ message: "База данных работает!", time: result.rows[0].now });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(PORT, () => {
